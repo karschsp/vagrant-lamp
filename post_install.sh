ba62866f34bfdd17ab40bbdabe669506dca0a242
Steve Karsch <steve@stevekarsch.com>
2014-03-13 16:09:11 -0400

    Add drush to path. PHP display_errors = On.

--- post_install.sh
+++ post_install.sh
@@ -5,4 +5,5 @@ sudo service httpd restart
 sudo service memcached restart
 curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
+sed -i '1i export PATH="$HOME/.composer/vendor/bin:$PATH"' $HOME/.bashrc
 composer global require drush/drush:6.*
