04fdb8bbc0d537069cf3a2387e7f82e5af379d2c
Steve Karsch <steve@stevekarsch.com>
2014-03-18 12:15:08 -0400

    Add post_install.sh for drush and to grant mysql access from anywhere.

--- Vagrantfile
+++ Vagrantfile
@@ -79,6 +79,9 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
     puppet.manifests_path = "puppet/manifests"
     puppet.manifest_file  = "site.pp"
   end
+  
+  config.vm.provision :shell, :path => "post_install.sh"
+  
 
   # Enable provisioning with chef solo, specifying a cookbooks path, roles
   # path, and data_bags path (all relative to this Vagrantfile), and adding
--- post_install.sh
+++ post_install.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-sudo service mysqld restart
-sudo service httpd restart
-sudo service memcached restart
-curl -sS https://getcomposer.org/installer | php
+cd ~
+sudo curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
 sed -i '1i export PATH="$HOME/.composer/vendor/bin:$PATH"' $HOME/.bashrc
-composer global require drush/drush:6.*
+sudo /usr/local/bin/composer global require drush/drush:6.*
+mysql -u root -e "GRANT ALL ON *.* TO 'root'@'%';FLUSH PRIVILEGES;"
