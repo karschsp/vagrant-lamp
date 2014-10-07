f22bf25c91b21d952ed0cb1e4823d5bdf9ca3412
Steve Karsch <steve@stevekarsch.com>
2014-10-06 22:17:09 -0400

    Typo in post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -2,7 +2,7 @@
 
 cd /home/vagrant
 sudo -u vagrant curl -sS https://getcomposer.org/installer | php
-vagrant mv composer.phar /usr/local/bin/composer
+mv composer.phar /usr/local/bin/composer
 sudo -u vagrant sed -i '1i export PATH="/home/vagrant/.composer/vendor/bin:$PATH"' /home/vagrant/.bashrc
 sudo -u vagrant /usr/local/bin/composer global require drush/drush:6.*
 mysql -u root -e "GRANT ALL ON *.* TO 'root'@'%';FLUSH PRIVILEGES;"
